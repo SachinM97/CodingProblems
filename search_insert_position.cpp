81a3d0447bbf894d0d2ed5d18ab5ea09ec4fe97f
SachinCTX <sachin.mehndiratta@cleartax.in>
2021-10-27 23:54:02 +0530

    New codes

---
+++ search_insert_position.cpp
@@ -0,0 +1,51 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+// Example 2:
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+// Example 3:
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+// Example 4:
+
+// Input: nums = [1,3,5,6], target = 0
+// Output: 0
+// Example 5:
+
+// Input: nums = [1], target = 0
+// Output: 0
+
+class Solution
+{
+public:
+    int searchInsert(vector<int> &nums, int target)
+    {
+        std::vector<int>::iterator it;
+        it = std::find(nums.begin(), nums.end(), target);
+        if (it != nums.end())
+            return it - nums.begin();
+        else
+        {
+            if (target == 0 && nums[0] > 0)
+                return 0;
+            else
+            {
+                long int x = nums.size();
+                for (int i = 0; i < x; i++)
+                {
+                    if (nums[i] > target)
+                        return i;
+                }
+            }
+        }
+        return nums.size();
+    }
+};
